c16554fd06ff9ca26131d82ae43d8c8dc437ae3b
redgum2k4@gmail.com <redgum2k4@gmail.com>
2016-08-12 12:33:07 +0100

    Remove local hacked TLRestClient impl, add dependency to shared project.
    Updated for RestClient refactor.
    added hibernate auto ddl create-drop so each run starts with a new db.

--- src/main/java/org/crm/tlcrmspa/IntegrationConfig.java
+++ src/main/java/org/crm/tlcrmspa/IntegrationConfig.java
@@ -1,27 +1,25 @@
 package org.crm.tlcrmspa;
 
-import javax.annotation.PostConstruct;
-
-import org.crm.tlcrmspa.service.ClientService;
-import org.crm.tlcrmspa.trafficlive.TrafficLiveRestClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import com.deltek.integration.trafficlive.service.TrafficLiveRestClient;
+
 @Configuration
 public class IntegrationConfig {
-
-	@Bean
-	public TrafficLiveRestClient trafficLiveRestClient() {
-		return new TrafficLiveRestClient(restTemplate());
-	}
 	
 	@Bean
-	public RestTemplate restTemplate() {
+	private RestTemplate restTemplate() {
 		TestRestTemplate basicAuthRestTemplate = new TestRestTemplate("simonstewart@deltek.com", "turK0QoPU5gkO6usNGfZcYcBpMEIaI0nUNLZQQSA");
 		return basicAuthRestTemplate;
 	}
-
+	
+	@Bean
+	public TrafficLiveRestClient restClient() {
+		TrafficLiveRestClient tlClient = new TrafficLiveRestClient(restTemplate(), "https://stage-api.sohnar.com/TrafficLiteServer/openapi");
+		return tlClient;
+	}
+	
 }
--- src/main/java/org/crm/tlcrmspa/TrafficLiveDataLoad.java
+++ src/main/java/org/crm/tlcrmspa/TrafficLiveDataLoad.java
@@ -1,24 +1,19 @@
 package org.crm.tlcrmspa;
 
-import javax.annotation.PostConstruct;
-
 import org.crm.tlcrmspa.domain.Client;
 import org.crm.tlcrmspa.service.ClientService;
-import org.crm.tlcrmspa.trafficlive.ClientCRMEntryTO;
-import org.crm.tlcrmspa.trafficlive.ClientPagedResultsTO;
-import org.crm.tlcrmspa.trafficlive.TrafficLiveRestClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.deltek.integration.trafficlive.domain.ClientCRMEntryTO;
+import com.deltek.integration.trafficlive.domain.ClientPagedResultsTO;
+import com.deltek.integration.trafficlive.service.TrafficLiveRestClient;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import io.gatling.core.result.writer.RequestMessage;
 
 @Controller
 public class TrafficLiveDataLoad {
@@ -34,16 +29,16 @@ public class TrafficLiveDataLoad {
 
 	@RequestMapping(path="/api/action/load", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<ClientPagedResultsTO> loadClientFromTrafficLIVE(
-		@RequestParam(value = "currentPage", required = false, defaultValue="0") Integer currentPage,
+		@RequestParam(value = "currentPage", required = false, defaultValue="1") Integer currentPage,
 		@RequestParam(value = "windowSize", required = false, defaultValue="100") Integer windowSize) {
-		ClientPagedResultsTO result = trafficLiveRestClient.getClients(currentPage, windowSize);
+		ClientPagedResultsTO result = trafficLiveRestClient.client().getPage(currentPage, windowSize);
 		result.getResultList().forEach(i -> clientService.save(createClientFromTrafficClient(i)));
 		return ResponseEntity.ok().body(result);
 	}
 
 	@RequestMapping(path="/api/action/legacyload", method=RequestMethod.GET)
 	public void loadClientFromTrafficLIVE() {
-		ClientPagedResultsTO result = trafficLiveRestClient.getClients(0, 100);
+		ClientPagedResultsTO result = trafficLiveRestClient.client().getPage(0, 100);
 		result.getResultList().forEach(i -> clientService.save(createClientFromTrafficClient(i)));
 	}
 	
@@ -52,4 +47,9 @@ public class TrafficLiveDataLoad {
 		return localClient;
 	}
 
+	
+	public void ingestMaconomyClientsMaster() {
+		
+	}
+	
 }
